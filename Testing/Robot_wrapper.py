b4ea2c205b23a0a871a1c304fa34a8f06daebbdb
Leah Nuss <153524099+LeahN-lab@users.noreply.github.com>
2024-04-10 16:50:51 +0200

    littel changes

--- Testing/Robot_wrapper.py
+++ Testing/Robot_wrapper.py
@@ -70,14 +70,11 @@ class Robot():
 
     def GoToVialPickUp(self, vialNum):
         self.arm.set_gripper_position(550, wait=True)
-        self.arm.set_position(x=)
 
-
-        arm.set_position(x=500, y=0, z=200, roll=0, pitch=90, yaw=0, speed=20, wait=True)
-        arm.set_gripper_position(460, wait=True)
-        arm.set_position(x=365, y=470, z=450, roll=0, pitch=90, yaw=0, speed=20, wait=True)
-        arm.set_position(x=365, y=470, z=350, roll=0, pitch=90, yaw=0, speed=20, wait=True)
-
+        self.arm.set_position(x=500, y=0, z=200, roll=0, pitch=90, yaw=0, speed=20, wait=True)
+        self.arm.set_gripper_position(460, wait=True)
+        self.arm.set_position(x=365, y=470, z=450, roll=0, pitch=90, yaw=0, speed=20, wait=True)
+        self.arm.set_position(x=365, y=470, z=350, roll=0, pitch=90, yaw=0, speed=20, wait=True)
 
 def PickupPipette():
     arm.set_gripper_position(800, wait=True)
@@ -93,7 +90,6 @@ def VialPickUp():
     arm.set_gripper_position(205, wait=True)
     time.sleep(1)
     arm.set_position(x=395, y=476, z=200, roll=0, pitch=90, yaw=0, speed=50, wait=True)
-
 
 def VialToScale():
     arm.set_position(x=300, y=-400, z=200, roll=0, pitch=90, yaw=-90, speed=120, wait=True)
@@ -112,7 +108,6 @@ def ScaleToDispenser():
     arm.set_position(x=170, y=835, z=90, roll=0, pitch=90, yaw=90, speed=20, wait=True)
     time.sleep(1)
 
-
 def DispenserToScale():
     arm.set_position(x=170, y=735, z=90, roll=0, pitch=90, yaw=90, speed=20, wait=True)
     arm.set_position(x=300, y=483, z=133, roll=0, pitch=90, yaw=0, speed=120, wait=True)
@@ -120,7 +115,6 @@ def DispenserToScale():
     arm.set_position(x=-21, y=-627, z=200, roll=0, pitch=90, yaw=-90, speed=80, wait=True)
     arm.set_position(x=-21, y=-627, z=159, roll=0, pitch=90, yaw=-90, speed=20, wait=True)
     arm.set_gripper_position(500, wait=True)
-
 
 def ScaleToVialHolder():
     arm.set_gripper_position(205, wait=True)
@@ -136,91 +130,7 @@ def GoToInitialPosition():
     arm.set_position(x=400, y=0, z=133, roll=0, pitch=90, yaw=0, speed=120, wait=True)
 
 
-
-scale.tare() 
-VialPickUp()
-VialToScale()
-scale.tare()
-time.sleep(5)
-ScaleToDispenser()
-motor.move(500)
-time.sleep(9)
-DispenserToScale()
-weight1 = scale.getWeight()
-time.sleep(5)
-ScaleToVialHolder()
-GoToInitialPosition()
-
-print("The weight was: "+str(weight1)+"g")
-
-
-def Test():
-    GoToInitialPosition()
-    VialPickUp()
-    VialToScale()
-    PickUpVialScale()
-    ScaleToDispenser()
-    DispenserToScale()
-    PickUpVialScale()
-    ScaleToVialHolder()
-    GoToInitialPosition()
-
 Test()
-
-arm.set_gripper_position(205, wait=True)
-arm.set_position(x=387, y=484, z=133, roll=0, pitch=90, yaw=0, speed=50, wait=True)
-arm.set_position(x=387, y=484, z=70, roll=0, pitch=90, yaw=0, speed=10, wait=True)
-arm.set_gripper_position(400, wait=True)
-
-
-
-
-
-
-
-
-
-
-arm.set_gripper_position(400, wait=True)
-arm.set_position(x=380, y=483, z=150, roll=0, pitch=90, yaw=0, speed=10, wait=True)
-arm.set_servo_angle(angle=[0, 0, 0, -180, 90, 0], speed=10, wait=True)
-
-arm.set_position(x=380, y=483, z=133, roll=0, pitch=90, yaw=0, speed=20, wait=True)
-
-
-paths = [
-    [400, 200, 133, 0, 90, 0],
-    [300, 200, 133, 0, 90, 0],
-    [400, 483, 133, 0, 90, 0],
-]
-
-arm.set_position(*paths[0], speed=10, wait=True)
-
-
-
-
-arm.set_servo_angle(angle=[0, 0, 0, 0, -90, 0], speed=30, wait=True)
-arm.set_servo_angle(angle=[45, 0, 0, 0, -90, 0], speed=30, wait=True)
-
-arm.get_position()
-
-
-
-
-
-
-arm.set_position(x=400, y=600, z=200, roll=0, pitch=-90, yaw=-90, speed=50, wait=True)
-
-arm.set_position(x=000, y=500, z=200, roll=0, pitch=-90, yaw=-180, speed=30, wait=True)
-
-arm.set_position(x=400, y=400, z=200, roll=0, pitch=-90, yaw=-90, speed=30, wait=True)
-
-arm.set_position(x=100, y=0, z=150, roll=-180, pitch=0, yaw=0, speed=50, wait=True)
-
-
-arm.set_position(x=20, y=-300, z=50, roll=-180, pitch=0, yaw=0, speed=50, wait=True)
-
-time.sleep(10)
 
 #For remebering the trajectory
 arm = XArmAPI(ip, is_radian=True)
