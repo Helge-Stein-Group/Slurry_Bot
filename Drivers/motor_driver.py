454b6248cc783a61684decb8d951287e3e49eb57
Danika Heaney <danika.heaney@gmail.com>
2024-04-11 14:06:39 +0200

    motor

--- Drivers/motor_driver.py
+++ Drivers/motor_driver.py
@@ -1,80 +1,111 @@
 import serial
 import time
 import sys
+import threading
 
-class motorError(Exception):
+class MotorError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-class Motor():
-    def __init__(self,num,conf,baud,timeout):
+class Motor:
+    def __init__(self, num, conf, baud, timeout):
         """ Initializes the serial connection. """
         self.num = num
         self.conf = conf
         self.baud = baud
         self.timeout = timeout
         self.motor_moving = False
-        #try: 
-        self.ser = serial.Serial(conf, baud, timeout=timeout)
-        # except serial.SerialException:
-        #     print('Unable to connect to the motor or motor is already connected. Check the com port and try again.')
-        #     sys.exit(1) #Exiting the program if the motor is not connected.
+        self.ser = None
+        self.lock = threading.Lock()  # Thread lock for serial communication
+
+    def connect(self):
+        """ Opens the serial connection if not already open. """
+        if self.ser is None or not self.ser.isOpen():
+            self.ser = serial.Serial(self.conf, self.baud, timeout=self.timeout)
 
     def close(self):
         """ Closes the serial connection. """
-        self.ser.close()
-    
+        if self.ser is not None and self.ser.isOpen():
+            self.ser.close()
+
     def send(self, command):
-        """ Sends a command to the motor. Can be used to send custom commands."""
-        self.ser.write((command+"\n").encode('utf-8'))
-        
-    def checkConnection(self):
+        """ Sends a command to the motor. """
+        self.connect()
+        with self.lock:
+            self.ser.write((command + "\n").encode('utf-8'))
+
+    def check_connection(self):
         """ Checks if the motor is connected. """
-        return self.ser.isOpen()
-    
-    def setSpeed(self, speed):
+        return self.ser is not None and self.ser.isOpen()
+
+    def set_speed(self, speed):
         """ Sets the speed of the motor. """
-        if speed > 0 and speed < 1000:
+        if 0 < speed < 1000:
             self.send(str(self.num) + "V" + str(speed))
         else:
-            raise motorError('Speed must be between 0 and 1000')
-          
-    def stop(self):
-      """ Stops the motor. """
-      self.send(str(self.num) + "S")
+            raise MotorError('Speed must be between 0 and 1000')
 
-    def setAcceleration(self, acceleration):
+    def stop(self):
+        """ Stops the motor. """
+        self.send(str(self.num) + "S")
+
+    def set_acceleration(self, acceleration):
         """ Sets the acceleration of the motor. """
-        if acceleration > 0 and acceleration < 1000:
+        if 0 < acceleration < 1000:
             self.send(str(self.num) + "A" + str(acceleration))
         else:
-            raise motorError('Acceleration must be between 0 and 1000')
+            raise MotorError('Acceleration must be between 0 and 1000')
 
-    def move(self, steps, wait_for_motor:bool=True):
-        """ Moves the motor clockwise at the current speed for a set number of steps. """
+    def move(self, steps, wait_for_motor=True):
+        """ Moves the motor clockwise for a set number of steps. """
         self.motor_moving = True
         self.send(str(self.num) + "F" + str(steps))
         if wait_for_motor:
             self.wait_for_motor()
 
-    def moveUp(self, steps):
-        """ Moves the motor clockwise at the current speed for a set number of steps. """
-        self.motor_moving = True
-        self.send(str(self.num) + "F" + str(steps))
-
-    def moveDown(self, steps):
-        """ Moves the motor clockwise at the current speed for a set number of steps. """
-        self.motor_moving = True
-        self.send(str(self.num) + "F-" + str(steps))
-
     def wait_for_motor(self):
-        """ Wait for acknowledgment from the motor that task is finished """
+        """ Waits for acknowledgment from the motor that the task is finished. """
         while self.motor_moving:
-            response =  self.ser.readline().decode('utf-8').strip()
+            with self.lock:
+                response = self.ser.readline().decode('utf-8').strip()
             if response == 'MOTOR_FINISHED':
                 self.motor_moving = False
 
-    def moveCounterClockwise(self, steps):
-        """ Moves the motor counter-clockwise at the current speed for a set number of steps. """
+    def move_up(self, steps):
+        """ Moves the motor clockwise for a set number of steps. """
         self.motor_moving = True
-        self.send(str(self.num) + "B" + str(steps) + "\n")'''
+        self.send(str(self.num) + "F" + str(steps))
+
+    def move_down(self, steps):
+        """ Moves the motor counter-clockwise for a set number of steps. """
+        self.motor_moving = True
+        self.send(str(self.num) + "F-" + str(steps))
+
+    def move_counter_clockwise(self, steps):
+        """ Moves the motor counter-clockwise for a set number of steps. """
+        self.motor_moving = True
+        self.send(str(self.num) + "B" + str(steps))
+
+# Example usage:
+if __name__ == "__main__":
+    motor1 = Motor('0', 'COM5', 9600, timeout=20)
+    motor2 = Motor('1', 'COM5', 9600, timeout=20)
+
+    # Define functions for motor movements
+    def move_motor1():
+        motor1.move(100)
+
+    def move_motor2():
+        motor2.move(200)
+
+    # Create threads for each motor movement
+    thread1 = threading.Thread(target=move_motor1)
+    thread2 = threading.Thread(target=move_motor2)
+
+    # Start threads
+    thread1.start()
+    thread2.start()
+
+    # Wait for threads to finish
+    thread1.join()
+    thread2.join()
