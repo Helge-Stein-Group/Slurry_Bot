871965974d012b188c75e29ea7f467b2d97bf252
Danika Heaney <danika.heaney@gmail.com>
2024-04-15 16:12:55 +0200

    more paths

--- Testing/Robot_wrapper.py
+++ Testing/Robot_wrapper.py
@@ -1,33 +1,12 @@
 import time
 from xarm.wrapper import XArmAPI
 
-#maybe I should put this on a .json file later
-ip = '192.168.1.200'
 
 #This function is called when the error or warning code changes
 def hangle_err_warn_changed(item):
     print('ErrorCode: {}, WarnCode: {}'.format(item['error_code'], item['warn_code']))
 
 
-fixed_points = {
-    "InitialPoint": (-228, 0, 133, 0, 90, 180),
-    "VialStoragePoint": (-273, -100.5, 125, -160, 90, 110),
-    "Vial0": (-273, -100.5, -33, -160, 90, 110),
-    "Scale":(-287, 120, 92, -150, 90, -60),
-    "DispenserPoint": (-419.3, 104.5, 92, -64.9, 90, -106),
-    "Dispenser1": (-532.4, -96.8, 56.1, 127.7, 88.8, -52.4),
-    "VialRestPoint": (-363.6, -105.4, 35.8, 127.7, 88.8, -52.4),
-    "PipettePoint": (-497.3, -116.5, 192.5, -107.4, 88.2, 163.9),
-    "LiquidStoragePoint": (-275.2, -174, 214.1, -107.4, 88.2, 163.9),
-    "Binder": (600, 100, 250, 0, 90, 0),
-    "Solvent": (700, -50, 180, 0, 90, 0),
-    "Trash": (700, -50, 180, 0, 90, 0),
-    "MixerPoint": (-639.3, 0, 317.8, -107.4, 88.2, 163.9),     
-}
-#Vial0(x=-283.4, y=-100, z=-31.2, roll=-152.4, pitch=88.8, yaw=120.6, speed=20, wait=True)
-#GripperVialClose:185 Open:290, 
-#Rail Pos.:600mm (till Dispenser 1)
-#Rail Pos.:200mm
 class Robot():
     def __init__(self):
         self.arm = XArmAPI('192.168.1.200')
@@ -43,7 +22,6 @@ class Robot():
         self.arm.set_gripper_mode(0)
 
     def initialize(self):
-        
         self.arm.motion_enable(enable=True)
         self.arm.set_mode(0)
         self.arm.set_state(state=0)
@@ -68,12 +46,12 @@ class Robot():
         position = fixed_points[name]
         self.arm.set_position(x=position[0], y=position[1], z=position[2], roll=position[3], pitch=position[4], yaw=position[5], speed=speed, wait=wait)
     
-    def PickUpVial(self, vialnumber):
-        robot.arm.set_gripper_position(500, wait=True)
+    def PickUpVial(self):
+        robot.arm.set_gripper_position(400, wait=True)
         robot.GoTo_Point("VialStoragePoint", 100)
-        robot.GoTo_Point("Vial0", 30)
+        robot.GoTo_Point("Vial0", 60)
         robot.arm.set_gripper_position(185, wait=True)
-        robot.GoTo_Point("VialStoragePoint", 30)   
+        robot.GoTo_Point("VialStoragePoint", 60)  
 
     def VialToScale(self):
         self.arm.GoTo_point("scale")
@@ -179,105 +157,114 @@ class Robot():
 
 robot = Robot()
 
-#PickUpVial0
 robot.initialize()
 robot.GoTo_InitialPoint()
-
 robot.restart()
 
-#PickUpVial0
-robot.arm.set_gripper_position(500, wait=True)
-robot.GoTo_Point("VialStoragePoint", 100)
-robot.GoTo_Point("Vial0", 60)
-robot.arm.set_gripper_position(185, wait=True)
-robot.GoTo_Point("VialStoragePoint", 60)    
+robot.PickUpVial()  
 
 #VialToScale
 robot.arm.set_position(x=-280, y=-100, z=125, roll=-160, pitch=90, yaw=0, speed=60, wait=True)
 robot.GoTo_Point("Scale", 60)
-robot.arm.set_position(x=-287, y=250, z=92, roll=-150, pitch=90, yaw=-60, speed=60, wait=True)
-robot.arm.set_position(x=-287, y=250, z=52.5, roll=-150, pitch=90, yaw=-60, speed=30, wait=True)
-robot.arm.set_gripper_position(500, wait=True)
+robot.arm.set_position(x=-287, y=250, z=92, roll=-90, pitch=90, yaw=0, speed=60, wait=True)
+robot.arm.set_position(x=-287, y=250, z=52.5, roll=-90, pitch=90, yaw=0, speed=30, wait=True)
+robot.arm.set_gripper_position(400, wait=True)
 
 #ScaleTODispenser
 robot.arm.set_gripper_position(185, wait=True)
-robot.arm.set_position(x=-287, y=250, z=92, roll=-150, pitch=90, yaw=-60, speed=60, wait=True)
+robot.arm.set_position(x=-287, y=250, z=92, roll=-90, pitch=90, yaw=0, speed=60, wait=True)
 robot.GoTo_Point("Scale", 60)
 robot.GoTo_Point("DispenserPoint", 60)
 robot.GoTo_Point("Dispenser1", 40)
-robot.arm.set_position(x=-537, y=-98, z=75, roll= 127.7, pitch= 90, yaw=-52.4, speed=20, wait=True)
+robot.arm.set_position(x=-537, y=-98, z=75, roll= 180, pitch= 90, yaw=0, speed=20, wait=True)
 robot.GoTo_Point("Dispenser1", 20)
-
 
 #DispenserToRestPoint
 robot.GoTo_Point("DispenserPoint", 60)
 robot.arm.set_linear_track_pos(200, wait=True)
 robot.GoTo_Point("VialRestPoint", 60)
-robot.arm.set_position(x=-367.7, y=-104.2, z=-35, roll= 127.7, pitch= 90, yaw=-52.4, speed=20, wait=True)
+robot.arm.set_position(x=-367.7, y=-104.2, z=-35, roll= 180, pitch= 90, yaw=0, speed=20, wait=True)
 robot.arm.set_gripper_position(500, wait=True)
 
 #From rest to Picking up Pipette
 robot.GoTo_Point("VialRestPoint", 60)
 robot.arm.set_gripper_position(800, wait=False)
 robot.GoTo_Point("PipettePoint", 60)
-robot.arm.set_position(x=-420, y=-215, z=193.2, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True) #grabbing pipette
-robot.arm.set_gripper_position(480, wait=True)
-robot.arm.set_position(x=-420, y=-215, z=220, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True) #lifting pipette
+robot.arm.set_position(x=-367, y=-215, z=193.2, roll= 90, pitch= 90, yaw=0, speed=20, wait=True) #grabbing pipette
+robot.arm.set_gripper_position(450, wait=True)
+robot.arm.set_position(x=-367, y=-212, z=220, roll= 90, pitch= 90, yaw=0, speed=20, wait=True) #lifting pipette
+
+#Picking up pipette tip
+robot.GoTo_Point("PipettePoint", 20)
+
+robot.arm.set_gripper_position(380, wait=True)
+x1 = -263.5
+y1 = -123.2 
+robot.arm.set_position(x=x1, y=y1, z=250, roll= 90, pitch= 91, yaw=0, speed=60, wait=True)
+robot.arm.set_position(x=x1, y=y1, z=210, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+robot.arm.set_position(x=x1, y=y1, z=205, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+robot.arm.set_position(x=x1, y=y1, z=200, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+robot.arm.set_position(x=x1, y=y1, z=190, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+
+robot.arm.set_position(x=-263.5, y=-123.2, z=320, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+
+
+robot.arm.set_position(x=x1, y=-120.2, z=300, roll= 90, pitch= 91, yaw=0, speed=60, wait=True)
+
+
+robot.arm.set_position(x=-263.5, y=-123.2, z=300, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
+robot.arm.set_position(x=-263.5, y=-123.2, z=260, roll= 90, pitch= 91, yaw=0, speed=20, wait=True)
 
 #Pipette to Vial Rest Point
-robot.arm.set_position(x=-420, y=-215, z=193.2, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True)
+robot.arm.set_position(x=-367, y=-215, z=193.2, roll= 90, pitch= 90, yaw=0, speed=20, wait=True)
 robot.arm.set_gripper_position(800, wait=False)
 robot.GoTo_Point("PipettePoint", 60)
 robot.GoTo_Point("VialRestPoint", 60)
 
 #Vial Rest Point to Mixer Point
-robot.arm.set_position(x=-367.7, y=-104.2, z=-35, roll= 127.7, pitch= 90, yaw=-52.4, speed=20, wait=True)
+robot.arm.set_position(x=-367.7, y=-104.2, z=-35, roll= 180, pitch= 90, yaw=0, speed=20, wait=True)
 robot.arm.set_gripper_position(185, wait=True)
 robot.GoTo_Point("VialRestPoint", 30)
+robot.arm.set_position(x=-602, y=-60.2, z=19, roll= 90, pitch= 90, yaw=0, speed=30, wait=True)
+robot.arm.set_position(x=-602, y=-142, z=19, roll= 90, pitch= 90, yaw=0, speed=20, wait=True)
+robot.arm.set_position(x=-602, y=-142, z=-30, roll= 90, pitch= 90, yaw=0, speed=20, wait=True)
+robot.arm.set_gripper_position(480, wait=False)
+robot.arm.set_position(x=-602, y=-30, z=19, roll= 90, pitch= 90, yaw=0, speed=30, wait=True)
 
 
-robot.GoTo_Point("MixerPoint", 20)
-robot.arm.set_position(x=-420, y=20, z=40, roll= -18, pitch= 88.2, yaw=163.9, speed=20, wait=True)
-robot.arm.set_linear_track_pos(20, wait=True)
+
+#Turn on mixer
+robot.arm.set_gripper_position(800, wait=False)
+
+robot.arm.set_position(x=-602, y=-30, z=300, roll= 90, pitch= 90, yaw=0, speed=20, wait=True)
+
+robot.arm.set_position(x=-610, y=-82.5, z=307.8, roll= 90, pitch= 90, yaw=0, speed=20, wait=True)
+
+robot.arm.set_gripper_position(500, wait=False)
 
 
 
-
-robot.arm.set_position(x=-660, y=-90, z=17, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True)
-
-
--497.3, -116.5, 192.5, -107.4, 88.2, 163.9
-
-robot.arm.set_position(x=-410, y=-119, z=192.5, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True)
-robot.arm.set_position(x=-420, y=-215, z=193.2, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True)
-robot.arm.set_position(x=-420, y=-215, z=220, roll= -107.4, pitch= 90, yaw=163.9, speed=20, wait=True)
-
-
-robot.arm.set_position(x=-367.7, y=-104.2, z=-35, roll= 127.7, pitch= 90, yaw=-52.4, speed=20, wait=True)
-
-
-robot.arm.set_position(x=-287, y=120, z=92, roll= -150, pitch= 90, yaw=-60, speed=40, wait=True)
-
-robot.arm.set_position(x=-400, y=50, z=92, roll= 120, pitch= 90, yaw=-60, speed=40, wait=True) #DispenserPoint
-
-robot.arm.set_position(x=-537, y=-97.5, z=40, roll= 127.7, pitch= 90, yaw=-52.4, speed=40, wait=True)
-robot.arm.set_position(x=-537, y=-98, z=75, roll= 127.7, pitch= 90, yaw=-52.4, speed=20, wait=True)
 
 
 
 
 fixed_points = {
-    "InitialPoint": (-228, 0, 133, 0, 90, 180),
-    "VialStoragePoint": (-273, -100.5, 125, -160, 90, 110),
-    "Vial0": (-273, -100.5, -33, -160, 90, 110),
-    "Scale":(-287, 120, 92, -150, 90, -60),
-    "DispenserPoint": (-400, 50, 92, 120, 90, -60),
-    "Dispenser1": (-537, -98, 40, 127.7, 90, -52.4),
-    "VialRestPoint": (-367.7, -104.2, 92, 127.7, 90, -52.4),
-    "PipettePoint": (-420, -110, 193.2, -107.4, 90, 163.9),
+    "InitialPoint": (-228, 0, 133, 180, 90, 0),
+    "VialStoragePoint": (-273, -100.5, 125, 90, 90, 0),
+    "Vial0": (-273, -100.5, -33, 90, 90, 0),
+    "Scale":(-287, 120, 92, -90, 90, 0),
+    "DispenserPoint": (-400, 50, 92, 180, 90, 0),
+    "Dispenser1": (-537, -98, 40, 180, 90, 0),
+    "VialRestPoint": (-367.7, -104.2, 92, 180, 90, 0),
+    "PipettePoint": (-370, -110, 250, 90, 90, 0),
     "LiquidStoragePoint": (-275.2, -174, 214.1, -107.4, 88.2, 163.9),
     "Binder": (600, 100, 250, 0, 90, 0),
     "Solvent": (700, -50, 180, 0, 90, 0),
     "Trash": (700, -50, 180, 0, 90, 0),
-    "MixerPoint": (-639.3, 0, 317.8, -107.4, 88.2, 163.9),     
+    "MixerPoint": (-639.3, 0, 317.8, -107.4, 88.2, 163.9),  
+       
 }
+
+
+
+#    "PipettePoint": (-370, -110, 193.2, -107.4, 90, 163.9),
