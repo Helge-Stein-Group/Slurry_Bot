1755006355e01ab296ae21400821946b7d888bb9
Danika Heaney <148461262+daheaney@users.noreply.github.com>
2024-04-15 16:47:51 +0200

    Update example_actionfile.py

--- Testing/example_actionfile.py
+++ Testing/example_actionfile.py
@@ -3,10 +3,44 @@ import sys
 import time
 
 from xarm.wrapper import XArmAPI
-from Robot_wrapper import Robot
+from Robot_wrapper import *
+from Drivers.scale_driver import *
+from Drivers.motor_driver import *
+from Drivers.pipette_driver import *
+
+#initializing and setting up all systems
+
+coms = {
+    scaleCom: "COM11",
+    motorsCom: "COM9",
+    pipetteCom: "COM6"
+}
+
+#Robot Connection
+robot = Robot()
+robot.initialize()
+robot.GoTo_InitialPoint()
+
+#Scale Connection
+scale = Scale(coms[scaleCom], 9600, 3)
+scale.connect()
+
+#Motors Connection
+motors = SerialConnection(coms[motorsCom], 9600, 3)
+homogenizer_motor = Motor(motors, 0)
+dispenser_motor = Motor(motors, 1)
+
+#Pipette Connection
+pipette = Pipette(coms[pipetteCom])
+pipette.initiate_rline()
 
 
-arm.connect()
+
+
+
+
+
+
 
 time.sleep(0.5)
 if arm.warn_code != 0:
