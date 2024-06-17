4a813ba1e128bd9a23f3738e894ed427629083cd
Leah Nuss <leah.nuss@tum.de>
2024-06-17 10:19:36 +0200

    plot

--- PaperTest/Plot.py
+++ PaperTest/Plot.py
@@ -2,18 +2,18 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.style as style
+import scienceplots
 from scipy.stats import linregress
 
-# Style auf 'science' mit dem Stil 'Nature' und 'No Latex' setzen
-style.use(['science', 'nature', 'no-latex'])
-style.available
+
+plt.style.use(['science', 'nature', 'no-latex'])
 
 # Daten aus der Kalibrierungsdatei lesen
 steps = []
 avg_weights = []
 std_weight = []
 std_rel_weight = []
-with open('PaperTest/report_cal_id_5_Tue_Jun__4_142036_2024.csv', 'r') as file:
+with open('/Users/leahnuss/Desktop/Slurry_Bot/PaperTest/report_cal_id_5_Tue_Jun__4_142036_2024.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         steps.append(float(row['Steps']))
@@ -30,31 +30,33 @@ r2_score = r_value**2
 
 # Plot1
 plt.figure(figsize=(3.5, 3.5)) 
-plt.errorbar(steps, avg_weights, yerr=std_weight, fmt='o', capsize=5, label='Measured Data')  # Fehlerbalken hinzufügen
+plt.errorbar(steps, avg_weights, yerr=std_rel_weight, fmt='o', capsize=5, label='Measured Weights with RSE')  # Fehlerbalken hinzufügen
 plt.plot(steps, line, color='red', label=f'Linear Fit (R2 Score: {r2_score:.2f})')  # Linearen Fit hinzufügen
 plt.xlabel('Steps [ ]')  # x-Achse mit Einheit
 plt.ylabel('Measured Weight [g]')  # y-Achse mit Einheit
-plt.title('Measurements with Error Bars')  # Titel des Plots
+plt.title('Calibration Measurement of Hard Carbon')  # Titel des Plots
 plt.legend()  # Legende hinzufügen
 plt.grid(True)  # Gitter hinzufügen
 plt.tight_layout()  # Layout anpassen, um Überlappungen zu vermeiden
 
 # Export Plot
-plt.savefig('CalibrationCurve_plot.svg', format='svg')
+#plt.savefig('CalibrationCurve_plot.svg', format='svg')
 plt.savefig('CalibrationCurve_plot.png', format='png')
+plt.show()
 
 # Plot2
+Percentage = [value * 100 for value in std_rel_weight]
 plt.figure(figsize=(3.5, 3.5))
-plt.plot(steps, std_weight, fmt='o', capsize=5, label='Standard error for amount of steps')
+plt.plot(steps, Percentage, label='Relative Standard Error(RSE)')
 plt.xlabel('Steps [ ]')  # x-Achse mit Einheit
-plt.ylabel('Standard error [g]')  # y-Achse mit Einheit
-plt.title('Measurements with Error Bars')  # Titel des Plots
+plt.ylabel('Relative Standard Error [%]')  # y-Achse mit Einheit
+plt.title('Relative Standard Error(RSE) for different amount of steps')  # Titel des Plots
 plt.legend()  # Legende hinzufügen
 plt.grid(True)  # Gitter hinzufügen
 plt.tight_layout()  # Layout anpassen, um Überlappungen zu vermeiden
 
 # Export Plot
-plt.savefig('Error_plot.svg', format='svg')
+#plt.savefig('Error_plot.svg', format='svg')
 plt.savefig('Error_plot.png', format='png')
 
 # Anzeigen
